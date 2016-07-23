84200478d23829a962276b7178fb7382fb137b13
Thambaru Wijesekara <thambaru@gmail.com>
2016-07-23 08:43:44 +0530

    Add radio option to select way.

--- js/main.js
+++ js/main.js
@@ -1,9 +1,19 @@
-String.prototype.replaceObj = function (textObj) {
+String.prototype.strictToNormal = function (textObj) {
     var replaceString = this;
     var regex;
     for (var key in textObj) {
 	regex = new RegExp(key, "g");
 	replaceString = replaceString.replace(regex, textObj[key]);
+    }
+    return replaceString;
+};
+
+String.prototype.normalToStrict = function (textObj) {
+    var replaceString = this;
+    var regex;
+    for (var key in textObj) {
+	regex = new RegExp(textObj[key], "g");
+	replaceString = replaceString.replace(regex, key);
     }
     return replaceString;
 };
@@ -16,7 +26,8 @@ jQuery(function ($) {
     });
 
     $('#convert-btn').click(function () {
-	var text = $('textarea').val();
+	var text = $('textarea').val(),
+		radio = $('#strictToNormal-radio');
 
 	var textObj = {
 	    "දෙයි": "දෙනවා",
@@ -25,13 +36,20 @@ jQuery(function ($) {
 	    "සැලසෙයි": "සැලසෙනවා",
 	    "කරයි": "කරනවා",
 	    "කෙරේ": "‍කරනවා",
+	    "කරවයි": "‍කරවනවා",
 	    "කළේය": "කළා",
 	    "කළේ ය": "කළා",
+	    "කිව්වේය":"කියනවා",
+	    "කියයි":"කියනවා",
 	    "හෙතෙම": "ඔහු",
 	    "ඇති බවයි": "ඇතැයි කියා යි",
-	    "ලබයි": "ලබනවා"
+	    "ලබයි": "ලබනවා",
+	    "ලබාදෙයි": "ලබා දෙනවා",
+	    "සපයයි": "සපයනවා",
+	    "තිබේ": "තිබෙනවා"
 	}
-	$('textarea').val(text.replaceObj(textObj));
+	console.log(radio.is(':checked'));
+	$('textarea').val(text[radio.is(':checked') ? 'strictToNormal' : 'normalToStrict'](textObj));
     });
 });
 
